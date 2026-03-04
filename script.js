b31e30583bde24907f954dee04636c56f321a545
inovtechnology9-cell <inov.technology9@gmail.com>
2026-03-04 20:07:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ const nav = document.getElementById('nav');
 toggle.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
+
+/* Scroll Animation */
+
+ScrollReveal().reveal('.hero h1', {
+    delay:300,
+    distance:'50px',
+    origin:'bottom',
+    duration:1000
+});
+
+ScrollReveal().reveal('.card', {
+    interval:200,
+    distance:'60px',
+    origin:'bottom',
+    duration:800
+});
+
+ScrollReveal().reveal('.product', {
+    interval:200,
+    distance:'60px',
+    origin:'top',
+    duration:800
+});
+
+ScrollReveal().reveal('form', {
+    delay:400,
+    distance:'50px',
+    origin:'bottom',
+    duration:1000
+});
